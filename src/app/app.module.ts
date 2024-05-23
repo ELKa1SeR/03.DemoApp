24c9ba3d437afd1403cf644c48519d28199b3480
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-05-23 16:51:16 +0200

    "commit error ()  111"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     HttpClientModule,
+
     GifsModule,
     SharedModule,
   ],
