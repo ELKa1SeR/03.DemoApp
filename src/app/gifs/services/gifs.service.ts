db3b768fb1fa89a784a0182b6ed10615374c404e
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-05-23 10:21:35 +0200

    "95"

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,52 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string= "sjflmAnsrlEpKm69LWxZoxAHKde5osVz";
+  serviceUrl: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag:string){
-    this._tagsHistory.unshift(tag);
+  private organizeHistory(tag : string){
 
-    console.log(this.tagsHistory);
+    tag = tag.toLowerCase();
+
+    // delete tag
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) =>
+        oldTag !==tag)
+    }
+
+    //insert tag
+    this._tagsHistory.unshift( tag );
+    // limit 10
+    this._tagsHistory = this.tagsHistory.splice(0,10);
+
+  }
+
+
+
+  searchTag(tag:string) : void {
+    if ( tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set("api_key", this.apiKey)
+    .set("limit", "10")
+    .set("q", tag)
+
+    this.http.get(`${ this.serviceUrl}/search`, {params})
+      .subscribe( resp => {
+        console.log(resp);
+      })
+
   }
 
 }
